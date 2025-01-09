b8eedda364d83edd82bf791d0d7a6d50c9200eee
akashkumar30698 <144131035+akashkumar30698@users.noreply.github.com>
2025-01-09 21:41:49 +0530

    huh

--- app/pages/api/portfolio/route.ts
+++ app/pages/api/portfolio/route.ts
@@ -1,9 +1,9 @@
 
 import { NextResponse,NextRequest } from 'next/server';
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
     try {
-        console.log(req,res)
+        console.log(req)
             return NextResponse.json({ success: 'Successfully Submitted' });
     } catch (error) {
         return NextResponse.json({ error: 'Error exchanging code for access token' ,err: error});
